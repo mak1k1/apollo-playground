312c1fe7a00b73949ae7e62c32ce29910362415e
mak1k1 <martin.cehelsky.ja@gmail.com>
2022-05-04 22:43:58 +0200

    Added mutation for author and book creation

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,11 +1,13 @@
 const books = [
   {
+    id: 1,
     title: "The Awakening",
-    author: { name: "Kate Chopin" },
+    author: { id: 1, name: "Kate Chopin" },
   },
   {
+    id: 2,
     title: "City of Glass",
-    author: { name: "Paul Auster" },
+    author: { id: 1, name: "Paul Auster" },
   },
 ]
 
@@ -13,6 +15,22 @@ const resolvers = {
   Query: {
     books: () => books,
   },
+  Mutation: {
+    addAuthor: (root, {input}) => {
+      return {
+      code: '200',
+      success: true,
+      message: 'Author created successfully',
+      author: {id: 3, name: input.name}}
+    },
+    addBook: (title) => {
+      return {
+        code: '200',
+        success: true,
+        message: 'Je to v pici drahi veriaci',
+        book: {id: 3, title: title}}   
+    },
+  }
 }
 
 module.exports = resolvers
--- src/graphql/typeDefs.js
+++ src/graphql/typeDefs.js
@@ -2,16 +2,48 @@ const { gql } = require("apollo-server-express")
 
 const typeDefs = gql`
   type Book {
+    id: ID!
     title: String
     author: Author
   }
   type Author {
+      id: ID!
       name: String
       books: [Book]
   }
 
   type Query {
     books: [Book]
+  }
+
+  input AuthorInput {
+    name: String
+    books: [ID]
+  }
+
+  type Mutation {
+    addAuthor(input: AuthorInput): AddAuthorMutationResponse
+    addBook(title: String): AddBookMutationResponse
+  }
+
+  interface MutationResponse {
+    code: String!
+    success: Boolean!
+    message: String!
+  }
+
+  type AddBookMutationResponse implements MutationResponse {
+    code: String!
+    success: Boolean!
+    message: String!
+    book: Book
+  }
+
+  type AddAuthorMutationResponse implements MutationResponse {
+    code: String!
+    success: Boolean!
+    message: String!
+    author: Author
   }
 `
 
